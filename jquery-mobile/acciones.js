43eebe43fb6f9fb6db515d7a3697d7347d04f709
AlexanderMichael <mari12c5898@gmail.com>
2015-06-08 11:53:58 -0500

    Sonidos Animales
    v 1.0

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(document).ready(function(){
+	//animales domesticos
+	var vaca = document.getElementById("vaca");
+	var gato = document.getElementById("gato");
+	var cerdo = document.getElementById("cerdo");
+	var pato = document.getElementById("pato");
+	var pollo = document.getElementById("pollo");
+	var perro = document.getElementById("perro");
+	//animales salvajes
+	var elefante = document.getElementById("elefante");
+	var foca = document.getElementById("foca");
+	var rana = document.getElementById("rana");
+	var leon = document.getElementById("leon");
+	var panda = document.getElementById("panda");
+	var mono = document.getElementById("mono");
+});
